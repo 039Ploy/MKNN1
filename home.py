c1bffb2110c1f652dea6050f0b9e2e5965ecd4f9
Thipnapa <155569922+039Ploy@users.noreply.github.com>
2024-11-19 14:56:59 +0700

    Update home.py

--- home.py
+++ home.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 
 st.title("🎈🧸Website Developing using Python🧸🎈")
-st.header("🚗🚗Website Developing using Python🚗🚗")
+st.header("🚗Website Developing using Python🚗")
 
 st.image('./img/Thipnapa.jpg')
-st.subheader("🌼Thipnapa Natswang")
+st.subheader("🌼Thipnapa Natswang🌼")
 
 dt=pd.read_csv('./data/iris-3.csv')
 st.header("ข้อมูลดอกไม้")
